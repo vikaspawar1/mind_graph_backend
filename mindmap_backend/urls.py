203bb7e4bf1f9024c43738f227105f09e80a5b82
vikas rahangdale <vikas.rahangdale@deepeigen.com>
2026-04-16 18:27:57 +0530

    done 16jvuds

--- mindmap_backend/urls.py
+++ mindmap_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from api.views import health  # Import health view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('health/', health), # Added root health check
 ]
